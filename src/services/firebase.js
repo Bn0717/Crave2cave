0bbaa80f9dd277ca3c63fcdc861415f2286ec3f7
Bryan Ngu <nguzhukiet0717@gmail.com>
2026-02-06 12:32:16 +0800

    Edit firebase functions

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,13 +5,13 @@ import { getFunctions, httpsCallable } from 'firebase/functions';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAutoUXgR5v9QSjEsDPkaEBxNzhrVd5r1c",
+  authDomain: "crave-2-cave.firebaseapp.com",
+  projectId: "crave-2-cave",
+  storageBucket: "crave-2-cave.firebasestorage.app",
+  messagingSenderId: "291986862315",
+  appId: "1:291986862315:web:5d4c3ee036c175ffa10eae",
+  measurementId: "G-JGR0HQV7SQ"
 };
 
 console.log("Check API Key:", firebaseConfig.apiKey);
